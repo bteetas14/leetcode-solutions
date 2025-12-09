3c454a19b3ef18e2ba4cc427f504f7aab1f83960
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2025-12-09 23:29:13 +0530

    Time: 466 ms (39.59%) | Memory: 73.6 MB (38.23%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,92 +1,56 @@
 class Solution {
 public:
-    int totalSum;
-    vector<vector<int>> findAllSubsetsSum(vector<int>& nums, int l, int r) {
-        int totLengthOfSubarray = r - l + 1;
-        vector<vector<int>> res(totLengthOfSubarray + 1);
-        for (int i = 0; i < (1 << totLengthOfSubarray); i++) {
-            int sum = 0, countOfChosenNos = 0;
-            for (int j = 0; j < totLengthOfSubarray; j++) {
-                if (i & (1 << j)) {
-                    sum += nums[l + j];
-                    countOfChosenNos++;
+    int minimumDifference(vector<int>& nums) {
+
+        int n = nums.size() / 2;
+        int total = accumulate(nums.begin(), nums.end(), 0);
+
+        vector<int> left(nums.begin(), nums.begin() + n);
+        vector<int> right(nums.begin() + n, nums.end());
+
+        // leftSums[k] → sums using k elements
+        vector<vector<int>> leftSums(n + 1), rightSums(n + 1);
+
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int cnt = 0, sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    cnt++;
+                    sum += left[i];
                 }
             }
-            res[countOfChosenNos].push_back(sum);
+            leftSums[cnt].push_back(sum);
         }
-        return res;
-    }
 
-    int minimumDifference(vector<int>& nums) {
-        int totalSum = accumulate(begin(nums), end(nums), 0);
-        int n = nums.size();
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int cnt = 0, sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    cnt++;
+                    sum += right[i];
+                }
+            }
+            rightSums[cnt].push_back(sum);
+        }
 
-        auto left = findAllSubsetsSum(nums, 0, n / 2 - 1);
-        auto right = findAllSubsetsSum(nums, n / 2, n - 1);
-        int target = totalSum / 2, ans = INT_MAX;
+        for (int i = 0; i <= n; i++)
+            sort(rightSums[i].begin(), rightSums[i].end());
 
-        //we can take (0 to n/2) length numbers from left
-        for (int i = 0; i <= n / 2; i++) {
-            //now we take rest - (n/2-i) length from right, we sort it to binary search
-            auto r = right[n / 2 - i];
-            sort(begin(r), end(r));
+        int ans = INT_MAX;
 
-            for (int curleftSum : left[i]) {
-                int needSumFromRight = target - curleftSum;
-                auto it = lower_bound(begin(r), end(r), needSumFromRight);
-                if (it != end(r))
-                    ans = min(ans, abs(totalSum - 2 * (curleftSum + *it)));
+        for (int k = 0; k <= n; k++) {
+            for (int s1 : leftSums[k]) {
+                int need = total / 2 - s1;
+                auto &v = rightSums[n - k];
+
+                auto it = lower_bound(v.begin(), v.end(), need);
+                if (it != v.end())
+                    ans = min(ans, abs(total - 2 * (s1 + *it)));
+                if (it != v.begin())
+                    ans = min(ans, abs(total - 2 * (s1 + *prev(it))));
             }
         }
+
         return ans;
     }
-
-
-    // int minimumDifference(vector<int>& arr) {
-
-    //     int totSum = 0;
-    //     int n = arr.size();
-
-    //     // Calculate the total sum of the array
-    //     for (int i = 0; i < n; i++) {
-    //         totSum += arr[i];
-    //     }
-
-    //     // Initialize a DP table to store the results of the subset sum problem
-    //     vector<vector<bool>> dp(n, vector<bool>(totSum + 1, false));
-
-    //     // Base case: If no elements are selected (sum is 0), it's a valid subset
-    //     for (int i = 0; i < n; i++) {
-    //         dp[i][0] = true;
-    //     }
-
-    //     // Initialize the first row based on the first element of the array
-    //     if (arr[0] <= totSum)
-    //         dp[0][totSum] = true;
-
-    //     // Fill in the DP table using a bottom-up approach
-    //     for (int ind = 1; ind < n; ind++) {
-    //         for (int target = 1; target <= totSum; target++) {
-    //             // Exclude the current element
-    //             bool notTaken = dp[ind - 1][target];
-
-    //             // Include the current element if it doesn't exceed the target
-    //             bool taken = false;
-    //             if (arr[ind] <= target)
-    //                 taken = dp[ind - 1][target - arr[ind]];
-
-    //             dp[ind][target] = notTaken || taken;
-    //         }
-    //     }
-
-    //     int mini = 1e9;
-    //     for (int i = 0; i <= totSum; i++) {
-    //         if (dp[n - 1][i] == true) {
-    //             // Calculate the absolute difference between two subset sums
-    //             int diff = abs(i - (totSum - i));
-    //             mini = min(mini, diff);
-    //         }
-    //     }
-    //     return mini;
-    // }
 };
