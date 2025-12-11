9ab97261faa3ace001cb69566b41c7c2cdb16694
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2025-12-12 01:48:44 +0530

    Time: 47 ms (23.58%) | Memory: 61.2 MB (16.17%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -21,12 +21,30 @@ public:
         return dp[ind][amount] = take + notTake;
     }
 
-    int change(int amount, vector<int>& coins) {
+    int change(int T, vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<long long>> dp(n, vector<long long>(T + 1, 0));
+        long long MOD = 1e15;
 
-        int n = coins.size();
+        // Initializing base condition
+        for (int i = 0; i <= T; i++) {
+            if (i % arr[0] == 0)
+                dp[0][i] = 1;
+        }
 
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return solve(n-1, amount, dp, coins);
-        
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 0; target <= T; target++) {
+                long long notTaken = dp[ind - 1][target];
+
+                long long taken = 0;
+                if (arr[ind] <= target)
+                    taken = dp[ind][target - arr[ind]];
+
+                dp[ind][target] = notTaken + taken;
+                dp[ind][target] %= MOD;
+            }
+        }
+
+        return dp[n - 1][T];
     }
 };
