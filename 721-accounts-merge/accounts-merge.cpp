14f986e41540e7830021dc414a7dc39ca2a930cc
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-01-21 00:23:05 +0530

    Time: 22 ms (94.19%) | Memory: 40.4 MB (52.66%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -6,6 +6,7 @@ public:
         rank.resize(n+1, 0);
         parent.resize(n+1);
         sizes.resize(n+1);
+
         for(int i=0;i<parent.size();i++){
             parent[i] = i;
             sizes[i] = 1;
@@ -13,17 +14,17 @@ public:
     }
 
     int findUParent(int node){
-        if(parent[node]==node){
+        if(parent[node] == node){
             return node;
         }
         return parent[node] = findUParent(parent[node]);
     }
 
     void unionByRank(int u, int v){
-        int ulp_v = findUParent(v);
         int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
 
-        if(ulp_v == ulp_u) return;
+        if(ulp_u == ulp_v) return;
 
         if(rank[ulp_u] > rank[ulp_v]){
             parent[ulp_v] = ulp_u;
@@ -38,10 +39,10 @@ public:
     }
 
     void unionBySize(int u, int v){
-        int ulp_v = findUParent(v);
         int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
 
-        if(ulp_v == ulp_u) return;
+        if(ulp_u == ulp_v) return;
 
         if(sizes[ulp_u] > sizes[ulp_v]){
             parent[ulp_v] = ulp_u;
@@ -58,38 +59,40 @@ class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
 
+        unordered_map<string, int> mailMapParent;
         int n = accounts.size();
         DisjointSet ds(n);
-        unordered_map<string, int> mailParentMap;
 
         for(int i=0;i<n;i++){
             for(int j=1;j<accounts[i].size();j++){
-                if(mailParentMap.find(accounts[i][j]) == mailParentMap.end()){
-                    mailParentMap[accounts[i][j]] = i;
+                string acc = accounts[i][j];
+
+                if(mailMapParent.find(acc) == mailMapParent.end()){
+                    mailMapParent[acc] = i;
                 }
                 else{
-                    int pNode = mailParentMap[accounts[i][j]];
-                    ds.unionBySize(pNode, i);
+                    int pNode = mailMapParent[acc];
+                    ds.unionByRank(pNode, i);
                 }
             }
         }
 
-        vector<vector<string>> mailMap(n);
-        for(auto it: mailParentMap){
+        vector<vector<string>> emails(n);
+        for(auto it:mailMapParent){
             string mail = it.first;
-            int prnt = ds.findUParent(it.second);
+            int parent = ds.findUParent(it.second);
 
-            mailMap[prnt].push_back(mail);
+            emails[parent].push_back(mail);
         }
 
         vector<vector<string>> ans;
         for(int i=0;i<n;i++){
-            if(mailMap[i].empty()) continue;
-            sort(mailMap[i].begin(), mailMap[i].end());
+            if(emails[i].empty()) continue;
+            sort(emails[i].begin(), emails[i].end());
 
             vector<string> temp;
             temp.push_back(accounts[i][0]);
-            for(auto it:mailMap[i]){
+            for(auto it: emails[i]){
                 temp.push_back(it);
             }
             ans.push_back(temp);
