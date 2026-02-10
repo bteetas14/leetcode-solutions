2ba10626302e278d84da11c9f14b5b7d4f1904b4
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-02-11 01:58:56 +0530

    Time: 11 ms (87.29%) | Memory: 50 MB (9.23%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,66 +1,60 @@
 class Solution {
 public:
-    vector<int> findNSE(vector<int>& arr){
-
+    vector<int> findNSE(vector<int> arr){
         stack<int> st;
         int n = arr.size();
-        vector<int> ans(n);
+        vector<int> nse(n);
 
         for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()] >= arr[i]){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
                 st.pop();
             }
             if(!st.empty()){
-                ans[i] = st.top();
+                nse[i] = st.top();
             }
             else{
-                ans[i] = n;
+                nse[i] = n;
             }
             st.push(i);
         }
-        return ans;
+        return nse;
     }
 
-    vector<int> findPSEE(vector<int>& arr){
-
+    vector<int> findPSEE(vector<int> arr){
         stack<int> st;
         int n = arr.size();
-        vector<int> ans(n);
+        vector<int> pse(n);
 
         for(int i=0;i<n;i++){
-            while(!st.empty() && arr[st.top()] > arr[i]){
+            while(!st.empty() && arr[st.top()]>arr[i]){
                 st.pop();
             }
             if(!st.empty()){
-                ans[i] = st.top();
+                pse[i] = st.top();
             }
             else{
-                ans[i] = -1;
+                pse[i] = -1;
             }
             st.push(i);
         }
-        return ans;
+        return pse;
     }
 
     int sumSubarrayMins(vector<int>& arr) {
 
+        vector<int> pse = findPSEE(arr);
         vector<int> nse = findNSE(arr);
-        vector<int> psee = findPSEE(arr);
+        int sum = 0, mod = 1e9 + 7;
 
-        int n = arr.size();
-        int mod = 1e9 + 7;
+        for(int i=0;i<arr.size();i++){
+            int left = i - pse[i];
+            int right = nse[i] - i;
 
-        int sum = 0;
+            long long freq = (left*right*1LL);
 
-        for(int i=0;i<n;i++){
-            int left = i-psee[i];
-            int right = nse[i]-i;
-
-            long long freq = left*right*1LL;
-
-            int val = (freq*arr[i]*1LL) % mod;
+            int val = (arr[i]*freq*1LL) % mod;
             sum = (sum + val) % mod;
         }
-        return sum;
+        return sum;    
     }
 };
