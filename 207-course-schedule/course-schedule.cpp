b22195a3a09b8578136951ca94fc572b168450f1
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-04-07 03:08:48 +0530

    Time: 11 ms (22.28%) | Memory: 20.2 MB (18.68%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    bool dfs(int node, int t, vector<int>& vis, vector<int>& pathVis, vector<vector<int>>& adj){
-        vis[node] = 1;
-        pathVis[node] = 1;
-
-        for(auto it:adj[node]){
-            if(vis[it]==0){
-                if(dfs(it, t, vis, pathVis, adj)) return true;
-            }
-            else if(pathVis[it]==1){
-                return true;
-            }
-        }
-        pathVis[node] = 0;
-        return false;
-    }
-
     bool canFinish(int t, vector<vector<int>>& prerequisites) {
 
-        vector<int> vis(t, 0);
-        vector<int> pathVis(t, 0);
         vector<vector<int>> adj(t);
+        vector<int> indegree(t, 0);
+        queue<int> q;
+        vector<int> topo;
 
-        for(auto it:prerequisites){
+        for(auto it: prerequisites){
             adj[it[0]].push_back(it[1]);
+            indegree[it[1]]++;
         }
 
         for(int i=0;i<t;i++){
-            if(vis[i]==0){
-                if(dfs(i, t, vis, pathVis, adj)==true) return false;
+            if(indegree[i]==0) q.push(i);
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            topo.push_back(node);
+
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
             }
         }
-        return true;
+        return topo.size()==t;
     }
 };
