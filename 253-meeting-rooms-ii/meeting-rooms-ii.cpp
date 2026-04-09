9dd9830507f63261db2407eeeba0efc7cdb6b145
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-04-10 02:08:36 +0530

    Time: 3 ms (54.55%) | Memory: 16.1 MB (81.69%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+
+        int n = intervals.size();
+        if(n==1) return 1;
+        sort(intervals.begin(), intervals.end());
+
+        priority_queue<int,
+            vector<int>,
+            greater<int>> pq;
+        
+        pq.push(intervals[0][1]);
+
+        for(int i=1;i<intervals.size();i++){
+            if(!pq.empty() && intervals[i][0] >= pq.top()){
+                pq.pop();
+            }
+            pq.push(intervals[i][1]);
+        }
+        return pq.size();
+    }
+};
