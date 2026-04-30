811883c8517ae08c3a2d7f5411e0452eb605a1cd
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-05-01 02:01:02 +0530

    Time: 95 ms (41.70%) | Memory: 88.1 MB (34.85%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -3,31 +3,26 @@ public:
     int longestSubarray(vector<int>& nums, int limit) {
 
         int n = nums.size();
-        priority_queue<pair<int, int>,
-            vector<pair<int, int>>,
-            greater<pair<int, int>>> minh;
-        
+
+        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>> minh;
         priority_queue<pair<int, int>> maxh;
-        int mini = 1e9, maxi = -1e9, start = 0, maxLen = -1e9;
+        int start = 0, maxLen = -1e9;
 
         for(int i=0;i<n;i++){
             minh.push({nums[i], i});
             maxh.push({nums[i], i});
 
-            mini = min(mini, minh.top().first);
-            maxi = max(maxi, maxh.top().first);
-
-            if(abs(maxh.top().first - minh.top().first) <= limit){
-                maxLen = max(maxLen, i-start+1);
+            if(abs(minh.top().first - maxh.top().first) <= limit){
+                maxLen = max(maxLen, i-start+1); 
             }
             else{
-                while(abs(maxh.top().first - minh.top().first) > limit){
+                while(abs(minh.top().first - maxh.top().first) > limit){
                     start++;
-                    while(maxh.top().second < start){
-                        maxh.pop();
-                    }
                     while(minh.top().second < start){
                         minh.pop();
+                    }
+                    while(maxh.top().second < start){
+                        maxh.pop();
                     }
                 }
             }
