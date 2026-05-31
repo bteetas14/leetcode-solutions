e26ff9b34a2e49a9f58b6aa2eaf9a2b204a05425
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-05-31 22:56:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (53.65%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
-    int solve(int row, int col, vector<vector<int>>& obstacleGrid, vector<vector<int>>&dp){
-        
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
+    int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
 
-        if(row>n-1 || col>m-1 || obstacleGrid[row][col]==1) return 0;
+        if(i==0 && j==0){
+            return 1;
+        }
 
-        if(row==n-1 && col==m-1) return 1;
+        if(i<0 || j<0 || grid[i][j]==1) return 0;
 
-        if(dp[row][col] != -1) return dp[row][col];
+        if(dp[i][j] != -1) return dp[i][j];
 
-        int down = solve(row+1, col, obstacleGrid, dp);
-        int right = solve(row, col+1, obstacleGrid, dp);
+        int top = solve(i-1, j, grid, dp);
+        int left = solve(i, j-1, grid, dp);
 
-        return dp[row][col] = down + right;
+        return dp[i][j] = top+left;
     }
 
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
 
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
 
-        if(obstacleGrid[0][0]==1 || obstacleGrid[n-1][m-1]==1) return 0;
+        if(grid[n-1][m-1]==1 || grid[0][0]==1) return 0;
 
-        // vector<vector<int>> dp(n, vector<int>(m, -1));
-        // return solve(0, 0, obstacleGrid, dp);
+        vector<vector<int>> dp(n, vector<int>(m, -1));
 
-        vector<vector<long long>> dp(n, vector<long long>(m, 0));
-        dp[n-1][m-1] = 1;
-
-        for(int row=n-1;row>=0;row--){
-            for(int col=m-1;col>=0;col--){
-
-                if(row==n-1 && col==m-1) continue;
-                if(obstacleGrid[row][col]==1) continue;
-
-                long long down = 0, right = 0;
-
-                if(row<n-1) down = dp[row+1][col];
-                if(col<m-1) right = dp[row][col+1];
-                             
-                dp[row][col] = (down+right);
-            }
-        }
-        return dp[0][0];
+        return solve(n-1, m-1, grid, dp);
+        
     }
 };
