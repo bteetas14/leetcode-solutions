1158424350d9fe8003fccfbdd9ead72a14b46bdd
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-06-06 04:02:49 +0530

    Time: 36 ms (21.95%) | Memory: 32.4 MB (15.70%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -3,28 +3,31 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
 
         int n = grid.size();
+        int m = grid[0].size();
         if(grid[0][0]!=0 || grid[n-1][n-1]!=0) return -1;
-        
-        vector<vector<int>> vis(n, vector<int>(n,0));
-        queue<pair<pair<int, int>, int>> q;
-        q.push({{0,0},1});
 
-        int dx[] = {-1, -1, 0, 1, 1, 1, 0, -1};
-        int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+        vector<int> dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+        vector<int> dy = {0, 1, 1, 1, 0, -1, -1, -1};
+
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        queue<pair<pair<int, int>, int>> q;
+        q.push({{0,0}, 1});
 
         while(!q.empty()){
-            auto [row, col] = q.front().first;
+            auto [r, c] = q.front().first;
             int dist = q.front().second;
             q.pop();
 
-            if(row==n-1 && col==n-1) return dist;
+            if(vis[r][c]==1) continue;
+            vis[r][c] = 1;
+
+            if(r==n-1 && c==m-1) return dist;
 
             for(int i=0;i<8;i++){
-                int nrow = row + dx[i];
-                int ncol = col + dy[i];
+                int nrow = r + dx[i];
+                int ncol = c + dy[i];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==0){
-                    vis[nrow][ncol] = 1;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==0){
                     q.push({{nrow, ncol}, dist+1});
                 }
             }
