98d44e1ea8ad4fdcf826c7aa973e15add070fb8c
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-06-20 01:35:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (47.83%) - LeetSync

--- 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.cpp
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     bool isMajorityElement(vector<int>& nums, int target) {
+
         int n = nums.size();
+        int low = 0, high = n-1, mid, ans=-1;
 
-        int low = 0, high = n - 1;
-        int first = -1;
+        while(low<=high){
+            mid = (low+high)/2;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                first = mid;
-                high = mid - 1;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            if(nums[mid]==target){
+                ans = mid;
+                high = mid-1;
+            }
+            if(nums[mid] < target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
             }
         }
-
-        if (first == -1) return false;
-
-        return first + n/2 < n && nums[first + n/2] == target;
+        if(ans==-1) return false;
+        int half = n/2;
+        if(ans+half<n && nums[ans]==nums[ans+half]) return true;
+        return false;
     }
 };
