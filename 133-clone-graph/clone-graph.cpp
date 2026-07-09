c30b1ac0b27771ac336c792f9fbf565a82c82118
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-07-09 21:19:11 +0530

    Time: 8 ms (5.28%) | Memory: 12 MB (81.02%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == NULL)
+            return NULL;
+
+        // Maps original node -> cloned node
+        unordered_map<Node*, Node*> cloneMap;
+
+        queue<Node*> q;
+
+        // Create clone of the starting node
+        Node* clonedStart = new Node(node->val);
+        cloneMap[node] = clonedStart;
+        q.push(node);
+
+        while (!q.empty()) {
+            Node* curr = q.front();
+            q.pop();
+
+            // Traverse all neighbors
+            for (Node* neighbor : curr->neighbors) {
+
+                // If neighbor hasn't been cloned yet
+                if (cloneMap.find(neighbor) == cloneMap.end()) {
+                    cloneMap[neighbor] = new Node(neighbor->val);
+                    q.push(neighbor);
+                }
+
+                // Connect cloned current node to cloned neighbor
+                cloneMap[curr]->neighbors.push_back(cloneMap[neighbor]);
+            }
+        }
+
+        return clonedStart;
+    }
+};
