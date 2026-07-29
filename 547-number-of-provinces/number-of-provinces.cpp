a13a391957e659ea676eec214c3725ece0de4151
Teetas Bhuiya <58104982+bteetas14@users.noreply.github.com>
2026-07-30 01:58:22 +0530

    Time: 4 ms (24.34%) | Memory: 21 MB (16.70%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    void dfs(int node, vector<int>& vis, vector<vector<int>>& adj){
+    void dfs(int node, vector<int>& vis, vector<vector<int>>& adjMat){
 
         vis[node] = 1;
 
-        for(auto it:adj[node]){
+        for(auto it:adjMat[node]){
             if(vis[it]==0){
-                dfs(it, vis, adj);
+                dfs(it, vis, adjMat);
             }
         }
     }
 
-    int findCircleNum(vector<vector<int>>& mat) {
+    int findCircleNum(vector<vector<int>>& isConnected) {
 
-        int n = mat.size();
+        int n = isConnected.size();
 
-        vector<int> vis(n, 0);
-        vector<vector<int>> adj(n);
+        vector<vector<int>> adjMat(n);
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i!=j && mat[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                if(isConnected[i][j] == 1 && i!=j){
+                    adjMat[i].push_back(j);
+                    adjMat[j].push_back(i);
                 }
             }
         }
 
+        vector<int> vis(n, 0);
         int cnt = 0;
 
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
+            if(vis[i] == 0){
                 cnt++;
-                dfs(i, vis, adj);
+                dfs(i, vis, adjMat);
             }
         }
         return cnt;
